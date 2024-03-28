558cfc7939347e0d637c3f50390e4c3c4bda7c74
m.kandel <mahmoudkandel980@gmail.com>
2024-03-28 23:11:58 +0200

    using vite (vanilla Js with typescript)

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        assetsDir: "assets",
+        rollupOptions: {
+            output: {
+                entryFileNames: "assets/[name].js",
+                chunkFileNames: "assets/[name].js",
+                assetFileNames: "assets/[name].[ext]",
+            },
+        },
+    },
+});
